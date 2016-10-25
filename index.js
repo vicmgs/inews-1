60182acbf107576fa0345ba04766b47d45ddf75d
Vorifin Layanto <vlayanto.at.dev@gmail.com>
2016-10-25 15:17:51 -0700

    Set env setting to run this app on test/dev mode

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var config = require('./server/config.js');
 var server = express();
 //local files
 var newsRouter = require('./server/routers/getnews.js');
@@ -9,7 +10,10 @@ server.use(express.static(__dirname + '/client'));
 
 server.set('port', (process.env.PORT || 5000) );
 
-mongoose.connect('mongodb://localhost/maoistMacaroons');
+var env = process.env.NODE_ENV;
+var mongoURI = env === 'TEST' ? config.MONGODB_LOCAL_URI : config.MONGODB_STAGING_URI;
+console.log(mongoURI);
+mongoose.connect(mongoURI);
 
 server.listen(server.get('port'), function () {
   console.log('listening');
