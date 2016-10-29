5a79f50ddc3127fb9d0c4f5e93c3e2582ea427f6
Victor Mu <vic.mgs@gmail.com>
2016-10-29 12:37:32 -0700

    Custom news calls Bing API after saving to database

--- client/app/customNews/customNews.js
+++ client/app/customNews/customNews.js
@@ -22,31 +22,26 @@ angular.module('inews.customNews', [])
   $scope.initializeCustom1 = function(query1) {
     CustomNewsService.edit({username: $window.localStorage['com.inews'], field: 'customnews1', value: query1})
     .then(function(resp){
-      // console.log(resp);
+      News.getBingNews(query1)
+      .then(function(data) {
+        $scope.custom1news = data.data.value;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
     })
-
-
-      // News.getBingNews(query1)
-      // .then(function(data) {
-      //   $scope.custom1news = data.data.value;
-      // })
-      // .catch(function(error) {
-      //   console.log(error);
-      // });
   };
 
   $scope.initializeCustom2 = function(query2) {
     CustomNewsService.edit({username: $window.localStorage['com.inews'], field: 'customnews2', value: query2})
     .then(function(resp){
-      // console.log(resp);
+      News.getBingNews(query2)
+      .then(function(data) {
+        $scope.custom2news = data.data.value;
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
     })
-
-    // News.getBingNews(query2)
-    // .then(function(data) {
-    //   $scope.custom2news = data.data.value;
-    // })
-    // .catch(function(error) {
-    //   console.log(error);
-    // });
   };
 });
