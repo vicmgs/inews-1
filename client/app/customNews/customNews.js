5a98d042c92167115e5530b4182fcd924d891db4
Victor Mu <vic.mgs@gmail.com>
2016-10-30 01:43:30 -0700

    first try to merge conflicts

--- client/app/customNews/customNews.js
+++ client/app/customNews/customNews.js
@@ -2,7 +2,7 @@
 
 angular.module('inews.customNews', [])
 
-.controller('customNewsController', function($scope, News, $location) {
+.controller('customNewsController', function($http, $scope, News, $location, CustomNewsService, $window) {
   $scope.custom1news = {};
   $scope.custom2news = {};
 
@@ -20,6 +20,8 @@ angular.module('inews.customNews', [])
   }
 
   $scope.initializeCustom1 = function(query1) {
+    CustomNewsService.edit({username: $window.localStorage['com.inews'], field: 'customnews1', value: query1})
+    .then(function(resp){
       News.getBingNews(query1)
       .then(function(data) {
         $scope.custom1news = data.data.value;
@@ -27,9 +29,12 @@ angular.module('inews.customNews', [])
       .catch(function(error) {
         console.log(error);
       });
+    })
   };
 
   $scope.initializeCustom2 = function(query2) {
+    CustomNewsService.edit({username: $window.localStorage['com.inews'], field: 'customnews2', value: query2})
+    .then(function(resp){
       News.getBingNews(query2)
       .then(function(data) {
         $scope.custom2news = data.data.value;
@@ -37,9 +42,43 @@ angular.module('inews.customNews', [])
       .catch(function(error) {
         console.log(error);
       });
+    })
   };
 
+  $scope.$on('signedin', function(event, arg) {
+    $http({
+      method: 'GET',
+      url: '/api/user/' + arg
+    })
+    .then(function(resp){
+      if (!!resp.data[0].customnews1) {
+        News.getBingNews(resp.data[0].customnews1)
+        .then(function(data) {
+          $scope.city1 = resp.data[0].customnews1;
+          $scope.custom1news = data.data.value;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+      }
 
-  $scope.initializeCustom1('Business');
-  $scope.initializeCustom2('Sports');
+      if(!!resp.data[0].customnews2) {
+        News.getBingNews(resp.data[0].customnews2)
+        .then(function(data) {
+          $scope.city2 = resp.data[0].customnews2;
+          $scope.custom2news = data.data.value;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+      }
+    })
+  });
+
+  $scope.$on('signedout', function(event) {
+    $scope.city1 = '';
+    $scope.city2 = '';
+    $scope.custom2news = {};
+    $scope.custom1news = {};
+  });
 });
